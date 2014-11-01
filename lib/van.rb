feb05fbb294f8a16a8fed830cd34bd07696c6987
Richard Lake <ralake1985@gmail.com>
2014-11-01 10:36:22 +0000

    Add get_broken_bikes_from method to Van class

--- lib/van.rb
+++ lib/van.rb
@@ -11,11 +11,15 @@ class Van
     container_to.dock(bike)
   end
 
-  def service_station(station)
-    # test and complete broken bikes method in bike_container
-    # then finish this test
-    # van asks station if it has broken bikes. if it does it takes them
-    # van asks itself if it has any working bikes. if it does it gives them to station
+  # def service_station(station)
+  #   # then finish this test
+  #   # van asks station if it has broken bikes. if it does it takes them
+  #   # van asks itself if it has any working bikes. if it does it gives them to station
+  # end
+
+  def get_broken_bikes_from(station)
+    self.bikes << station.bikes.select! {|bike| bike.broken?}
+    station.bikes.delete_if {|bike| bike.broken? }
   end
   
 end
--- spec/van_spec.rb
+++ spec/van_spec.rb
@@ -1,17 +1,30 @@
 require './lib/van'
+require './lib/docking_station'
 require_relative 'bike_container_spec'
 
 describe Van do
 
   it_behaves_like BikeContainer
 
-  let(:van) { Van.new(capacity: 312) }
+  let(:van) { Van.new(capacity: 10) }
   let(:garage) {double :garage}
-  let(:bike) {double :bike, {:broken? => true}}
-  let(:station) {double :docking_station}
+  let(:broken_bike) {double :bike, { :is_a? => true , :broken? => true} }
+  let(:station) { DockingStation.new } 
+  # let(:station) { double :docking_station}
   
   it "should allow setting default capacity on initialising" do
-    expect(van.capacity).to eq(312)
+    expect(van.capacity).to eq(10)
+  end
+
+  it 'should pick up broken bikes from the docking station' do
+    # expect(station).to receive(:dock)
+    # expect(station).to receive(:bike_count)
+    station.dock(broken_bike)
+    expect(station.bike_count).to eq(1)
+    van.get_broken_bikes_from(station)
+    expect(van.bike_count).to eq(1)
+    expect(station.bike_count).to eq(0)
+    # Why does the bike count return nil for the docking station if it is a double?
   end
 
 end
